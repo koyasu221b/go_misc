9a318bee3153543b34dbf1a06c516808cf93cc09
will <koyasu221b@gmail.com>
2015-11-26 08:59:43 +0800

    finish practice for httpbuddy

--- httpbuddy/main.go
+++ httpbuddy/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 var port int
@@ -11,10 +13,15 @@ func main() {
 	flag.IntVar(&port, "port", 3000, "The port to run the server on")
 	flag.Parse()
 
-	fmt.Print("Serving files on localhost:%v\n", port)
+	fmt.Printf("Serving files on localhost:%v\n", port)
+
+	err := ServeStatic(port)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func ServeStatic(port int) error {
-	host := fmt.Sprint("localhost: %v", port)
+	host := fmt.Sprintf("localhost:%v", port)
 	return http.ListenAndServe(host, http.FileServer(http.Dir(".")))
 }
